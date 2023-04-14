71309096a1a6c7987839e5cb9c5f8ef228b86720
chokoty <chokokty0816@gmail.com>
2023-04-14 14:01:35 +0900

    favicon 추가, hamberger메뉴 추가

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
-import HamburgerMenu from "./HamburgerMenu";
+// import HamburgerMenu from "./HamburgerMenu";
 import { SlControlRewind } from "react-icons/sl";
+import { Sling as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 export const Header = () => {
+    const [isOpen, setOpen] = useState(false);
     return (
         <header>
-            {/* <Link href="/" className="content">
-                <SlControlRewind className="logo" />
-                <span className="title">RE: FIND</span>
-            </Link> */}
-            <Link href="/support" className="content">
+            {/* <Link href="/support" className="content">
                 Supports
-            </Link>
-            <HamburgerMenu />
+            </Link> */}
+            <Hamburger
+                label="펼치기" // An ARIA label to improve accessibility.
+                size={20}
+                toggled={isOpen}
+                toggle={setOpen}
+            />
+            {/* <HamburgerMenu /> */}
         </header>
     );
 };
