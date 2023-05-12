c76aff1bbce8d371885a71a7d386481da3e2456b
chokoty <chokokty0816@gmail.com>
2023-05-12 16:45:44 +0900

    게시판 업데이트 링크 모바일로 임시 변경

--- data/board.js
+++ data/board.js
@@ -1,19 +1,27 @@
 const data = [
     {
         board: "이세돌 팬아트 게시판",
-        link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=344%26search.boardtype=I",
+        link: "https://m.cafe.naver.com/ca-fe/web/cafes/27842958/menus/344",
+        // link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=344%26search.boardtype=I",
+        // /ArticleList.nhn?search.clubid=27842958&search.menuid=344&search.boardtype=I"
     },
     {
         board: "통합 BEST 팬아트 게시판",
-        link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=551%26search.boardtype=I",
+        link: "https://m.cafe.naver.com/ca-fe/web/cafes/27842958/menus/551",
+
+        // link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=551%26search.boardtype=I",
     },
     {
         board: "금손 일러레의 방",
-        link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=552%26search.boardtype=L",
+        link: "https://m.cafe.naver.com/ca-fe/web/cafes/27842958/menus/552",
+
+        // link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=552%26search.boardtype=L",
     },
     {
         board: "왁타버스 불법 AI 팬아트",
-        link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=604%26search.boardtype=I",
+        link: "https://m.cafe.naver.com/ca-fe/web/cafes/27842958/menus/604",
+
+        // link: "https://cafe.naver.com/steamindiegame?iframe_url=/ArticleList.nhn%3Fsearch.clubid=27842958%26search.menuid=604%26search.boardtype=I",
     },
 ];
 
