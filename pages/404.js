f3495b52be93b10ceed7cb15b3f5d75b8fde9a6e
chokoty <chokokty0816@gmail.com>
2023-05-14 00:04:32 +0900

    404 페이지 추가

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { Text, Heading } from "@chakra-ui/react";
+export default function Custom404() {
+    return (
+        <div
+            style={{
+                margin: "0 auto",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+                width: "80%",
+            }}
+        >
+            <Heading as="h1" size="md" mb="20px">
+                404 - Page Not Found
+            </Heading>
+
+            <Image
+                src="/404-박쥐단.gif"
+                alt="404-박쥐단"
+                width={400}
+                height={400}
+            />
+        </div>
+    );
+}
