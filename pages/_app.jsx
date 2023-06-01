dfe5923704c8780beaefef6862d0402a3ccdf3b3
chokoty <chokokty0816@gmail.com>
2023-06-02 04:49:31 +0900

    구글 에널리틱스 설정

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,10 +3,27 @@ import MainLayout from "../components/layout/main-layout";
 import { Chakra } from "../styles/Chakra";
 import { Analytics } from "@vercel/analytics/react";
 import PlausibleProvider from "next-plausible";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 import "../styles/general.scss";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        router.events.on("hashChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+            router.events.off("hashChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <PlausibleProvider domain="https://re-find.xyz/">
             <Head>
@@ -14,6 +31,25 @@ export default function App({ Component, pageProps }) {
             </Head>
             <Chakra cookies={pageProps.cookies}>
                 <MainLayout>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <Script
+                        strategy="afterInteractive"
+                        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+                    />
+                    <Script
+                        id="gtag-init"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
                     <Component {...pageProps} />
                 </MainLayout>
             </Chakra>
