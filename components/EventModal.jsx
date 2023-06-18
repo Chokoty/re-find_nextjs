d3d32f223574a0754e7942065ce6506801f4cb47
chokoty <chokokty0816@gmail.com>
2023-06-19 01:17:21 +0900

    깜짝 이벤트(수정)

--- components/EventModal.jsx
+++ components/EventModal.jsx
@@ -48,11 +48,11 @@ const EventModal = () => {
                                 fontWeight: "bold",
                             }}
                         >
-                            리파인드 누적검색량 2만돌파 기념 깜짝이벤트
+                            리파인드 누적검색량 2만돌파 기념 깜짝 이벤트
                         </div>
-                        <ModalCloseButton />
+                        {/* <ModalCloseButton /> */}
                         <ModalBody>
-                            <Text>축하드립니다</Text>
+                            <Text>축하드립니다!</Text>
                             <Text>
                                 팬아트 검색을 이용하신 2만 번째 이파리로
                                 당첨되셨습니다!!!
@@ -63,8 +63,8 @@ const EventModal = () => {
                                 페이지입니다!!
                             </Text>
                             <Text color="#ef5a9a">
-                                본 팝업창은 1회만 뜨기 때문에 실수로 닫지 않도록
-                                해주세요!
+                                본 팝업창은 1회만 뜨기 때문에 실수로
+                                새로고침하거나 닫지 않도록 해주세요!
                             </Text>
                             <Divider orientation="horizontal" m="2" />
                             <Text>
@@ -97,16 +97,18 @@ const EventModal = () => {
                                 이벤트를 진행해 볼 예정이니 많은 관심
                                 부탁드립니다. 킹아!
                             </Text>
+                            <Divider orientation="horizontal" m="2" />
+                            <Text>나가시려면 새로고침 하시면 됩니다.</Text>
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button
+                            {/* <Button
                                 colorScheme="blue"
                                 mr={3}
                                 onClick={handleCloseModal}
                             >
                                 Close
-                            </Button>
+                            </Button> */}
                             {/* <Button variant="ghost">Secondary Action</Button> */}
                         </ModalFooter>
                     </ModalContent>
