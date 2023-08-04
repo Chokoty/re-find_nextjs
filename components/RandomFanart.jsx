a9987b343c38123dfa447fba8bd9e62cd6c8e7de
chokoty <chokokty0816@gmail.com>
2023-08-05 03:16:52 +0900

    Update: 랜덤 팬아트 버튼추가

--- components/RandomFanart.jsx
+++ components/RandomFanart.jsx
@@ -1,7 +1,10 @@
 import React, { use, useEffect, useState } from "react";
 import NextLink from "next/link";
+import NextImage from "next/image";
+
 import axios from "axios";
-import { Link, Image } from "@chakra-ui/react";
+import { Text, Link, Button } from "@chakra-ui/react";
+import { FaDice } from "react-icons/fa";
 
 const RandomFanart = ({ fanart }) => {
     const [isMobile, setIsMobile] = useState(true);
@@ -12,6 +15,7 @@ const RandomFanart = ({ fanart }) => {
     useEffect(() => {
         console.log("RandomFanart");
         console.log(fanart);
+        setFanart2(fanart);
         // fetchRandomFanart();
 
         const handleResize = () => {
@@ -31,9 +35,9 @@ const RandomFanart = ({ fanart }) => {
     }, [fanart2]);
     const url = isMobile
         ? "https://m.cafe.naver.com/ca-fe/web/cafes/27842958/articles/" +
-          fanart?.id +
+          fanart2?.id +
           "?fromList=true&menuId=344&tc=cafe_article_list"
-        : "https://cafe.naver.com/steamindiegame/" + fanart?.id;
+        : "https://cafe.naver.com/steamindiegame/" + fanart2?.id;
 
     const fetchRandomFanart = async () => {
         try {
@@ -53,6 +57,7 @@ const RandomFanart = ({ fanart }) => {
     const previewContainer = {
         display: "flex",
         flexDirection: "row",
+        justifyContent: "center",
         flexWrap: "wrap",
         marginTop: 16,
         marginBottom: 30,
@@ -60,24 +65,46 @@ const RandomFanart = ({ fanart }) => {
     const img = {
         display: "flex",
         height: "100%",
+        maxHeight: "400px",
         borderRadius: "1rem",
         objectFit: "cover",
         width: "80%",
+        boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+        marginBottom: "0.5rem",
+    };
+
+    const linkDiv = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        // height: "50%",
     };
 
     return (
         <div style={previewContainer} className="random-fanart">
-            {fanart && (
-                <Link href={url} passHref isExternal>
-                    <Image
+            {fanart2 && (
+                <Link href={url} passHref isExternal style={linkDiv}>
+                    <NextImage
                         style={img}
                         width={475}
                         height={475}
-                        src={fanart?.img_url}
+                        src={fanart2?.img_url}
                         alt="Description of the image"
                     />
+                    <Text>랜덤 팬아트 게시글 id: {fanart2?.id}</Text>
                 </Link>
             )}
+            <Button
+                colorScheme="yellow"
+                size="md"
+                mt="1.5rem"
+                onClick={fetchRandomFanart}
+            >
+                <FaDice />
+                &nbsp; 랜덤 팬아트
+            </Button>
         </div>
     );
 };
