ba1c86b85225f57cf47dc4b0485b9effa80442aa
chokoty <chokokty0816@gmail.com>
2023-08-05 13:29:46 +0900

    Update: 랜덤 팬아트 버튼추가12

--- components/RandomFanart.jsx
+++ components/RandomFanart.jsx
@@ -38,7 +38,7 @@ const RandomFanart = () => {
     //       "?fromList=true&menuId=344&tc=cafe_article_list"
     //     : "https://cafe.naver.com/steamindiegame/" + fanart2?.id;
     const handleLoad = async () => {
-        await new Promise((r) => setTimeout(r, 500));
+        await new Promise((r) => setTimeout(r, 700));
         setIsLoaded(true);
     };
 
