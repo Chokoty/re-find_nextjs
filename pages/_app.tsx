444e07676aedf1af7816638f4a2d3d539260fe84
chokoty <chokokty0816@gmail.com>
2023-08-11 15:27:34 +0900

    Feat: GTM 추가하기8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -72,7 +72,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WFK6LGVJ');`,
                         }} */}
-                    />
+                    {/* /> */}
                     <Component {...pageProps} />
                 </MainLayout>
             </Chakra>
