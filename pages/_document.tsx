9eb2400ef6471840540396d2070305892598e718
chokoty <chokokty0816@gmail.com>
2023-08-11 11:22:16 +0900

    Feat: GTM 추가하기4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -235,15 +235,17 @@ export default function Document() {
                     data-domain="re-find.xyz"
                     src="https://plausible.io/js/script.js"
                 ></script>
-                <Script id="google-tag-manager" strategy="afterInteractive">
-                    {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '
-    GTM-WFK6LGVJ');`}
-                </Script>
+                <Script
+                    id="google-tag-manager"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-XXXXXXX');`,
+                    }}
+                ></Script>
             </Head>
             <body>
                 <ColorModeScript
