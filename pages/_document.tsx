20edfed3c94fbf1b91de14417962cc230db179f7
chokoty <chokokty0816@gmail.com>
2023-08-11 10:07:36 +0900

    Feat: GTM 추가하기

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -234,6 +234,15 @@ export default function Document() {
                     data-domain="re-find.xyz"
                     src="https://plausible.io/js/script.js"
                 ></script>
+                <script id="google-tag-manager" strategy="afterInteractive">
+                    {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', '
+    GTM-WFK6LGVJ');`}
+                </script>
             </Head>
             <body>
                 <ColorModeScript
@@ -241,6 +250,15 @@ export default function Document() {
                 />
                 <Main />
                 <NextScript />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=
+            GTM-WFK6LGVJ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+                    }}
+                />
             </body>
         </Html>
     );
