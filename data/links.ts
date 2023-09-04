ab57ee1a422e1d824459e598b1aacf581ca0cf24
chokoty <chokokty0816@gmail.com>
2023-09-05 02:54:43 +0900

    Fix: 검색결과 게시글 모바일링크 수정

--- data/links.ts
+++ data/links.ts
@@ -1,6 +1,7 @@
 export const links = {
   mobile: {
-    article: 'https://m.cafe.naver.com/ca-fe/web/cafes/27842958/articles/',
+    // article: 'https://m.cafe.naver.com/ca-fe/web/cafes/27842958/articles/',
+    article: 'https://m.cafe.naver.com/steamindiegame/',
     menu: 'https://m.cafe.naver.com/ca-fe/web/cafes/27842958/menus/',
     member: 'https://m.cafe.naver.com/ca-fe/web/cafes/27842958/members/',
   },
