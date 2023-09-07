7b282e2f5a0c7f960d549cc7c0f518707e688123
chokoty <chokokty0816@gmail.com>
2023-09-08 02:39:04 +0900

    Feat: 작가 프로필 페이지 뷰 추가

--- components/MansonryView.tsx
+++ components/MansonryView.tsx
@@ -1,54 +1,81 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import NextImage from 'next/image';
-import { Box, Image, Link } from '@chakra-ui/react';
+import { Text, Box, Link } from '@chakra-ui/react';
+import Bricks from 'bricks.js';
 
 import { useResponsiveLink } from '../hook/useResponsiveLink';
 
 const MasonryView = ({ artworks }) => {
   const article_link = useResponsiveLink('', 'article');
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const instance = Bricks({
+      container: containerRef.current,
+      packed: 'data-packed',
+      sizes: [
+        { columns: 2, gutter: 16 },
+        { mq: '756px', columns: 3, gutter: 16 }, // 252*3 +4
+        { mq: '1008px', columns: 4, gutter: 16 }, // 252*4=1008
+        { mq: '1260px', columns: 5, gutter: 16 }, // 252*5=1260
+        { mq: '1528px', columns: 6, gutter: 16 }, // 252*6=1512
+        { mq: '1792px', columns: 7, gutter: 16 }, // 252*7=1764
+      ],
+    });
+
+    const handleResize = () => {
+      instance.resize(true).pack();
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    instance.resize(true).pack();
+
+    return () => {
+      instance.resize(false);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [artworks]);
+  // useEffect(() => {
+  //   const calculateColumns = () => {
+  //     const containerWidth = containerRef.current.offsetWidth;
+  //     return Math.floor(containerWidth / 236);
+  //   };
+
+  //   // const instance = Bricks({
+  //   //   container: containerRef.current,
+  //   //   packed: 'data-packed',
+  //   //   sizes: [
+  //   //     { columns: 2, gutter: 10 },
+  //   //     { mq: '768px', columns: 3, gutter: 16 },
+  //   //     { mq: '1024px', columns: 4, gutter: 16 },
+  //   //   ],
+  //   // });
+  //   const instance = Bricks({
+  //     container: containerRef.current,
+  //     packed: 'data-packed',
+  //     sizes: [{ columns: calculateColumns(), gutter: 16 }],
+  //   });
+
+  //   instance.resize(true).pack();
+
+  //   return () => {
+  //     instance.resize(false);
+  //   };
+  // }, [artworks]);
+
   return (
-    <Box
-      padding={4}
-      w="100%"
-      maxW="900px"
-      mx="auto"
-      sx={{ columnCount: [1, 2, 3], columnGap: '8px' }}
-      background="#f32a2a"
-    >
+    <Box ref={containerRef} w="100%" mx="auto">
       {artworks.map((artwork) =>
         !artwork.deleted ? (
-          // <Image
-          //   key={src?.id}
-          //   w="100%"
-          //   borderRadius="xl"
-          //   mb={2}
-          //   display="inline-block"
-          //   src={src}
-          //   alt="Alt"
-          // />
-          // <NextImage
-          //   key={src?.id}
-          //   alt={src?.title}
-          //   width={300}
-          //   height={300}
-          //   src={src?.img_url}
-          //   unoptimized
-          // />
           <Box
-            w="100%"
-            borderRadius="xl"
-            mb={2}
+            w="236px"
+            // borderRadius="xl"
+            pb="16px"
             display="inline-block"
-            // src={src}
-            // alt="Alt"
             key={artwork.id}
-            // m="8px"
-            // w="252px"
-            // h="234px"
-            // alignItems="center"
-            // borderRadius="1rem"
-            // overflow="hidden"
-            // flexWrap="wrap"
+            // position="relative"
+            // _hover={{ filter: 'brightness(70%)' }}
           >
             <Link
               href={
@@ -57,21 +84,52 @@ const MasonryView = ({ artworks }) => {
                   : article_link + artwork.url.split('/').pop()
               }
               isExternal
+              position="relative"
+              target="_blank"
+              rel="noopener noreferrer" // 보안상의 이유료 이 부분도 추가합니다.
             >
               <NextImage
                 alt={artwork.title}
-                width={300}
-                height={300}
+                width={236}
+                height={236}
                 style={{
                   objectFit: 'cover',
                   objectPosition: 'center top',
-                  // objectPosition: 'center -50px',
                   width: '100%',
                   height: '100%',
+                  borderRadius: '1rem',
                 }}
                 src={artwork.img_url}
                 unoptimized
               />
+              <Box
+                position="absolute"
+                top={0}
+                right={0}
+                bottom={0}
+                left={0}
+                borderRadius="1rem"
+                _hover={{
+                  backgroundColor: 'rgba(0, 0, 0, 0.3)', // 검은색의 30% 투명도
+                }}
+                zIndex={1}
+              />
+            </Link>
+            <Link
+              href={
+                artwork.url === ''
+                  ? '#'
+                  : article_link + artwork.url.split('/').pop()
+              }
+              isExternal
+              _hover={{ textDecoration: 'none' }}
+              target="_blank"
+              rel="noopener noreferrer" // 보안상의 이유료 이 부분도 추가합니다.
+            >
+              <Text fontWeight={500} pt="0.5rem" pb="1rem">
+                {artwork.title}
+                {/* {artwork.title} - {artwork.date.split(' ')[0].slice(0, -1)} */}
+              </Text>
             </Link>
           </Box>
         ) : null
