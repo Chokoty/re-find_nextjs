24c3f86b2d7576042958592abda041224dac5b2d
chokoty <chokokty0816@gmail.com>
2023-09-16 13:49:01 +0900

    Fix: 모바일 화면사이즈 버그 해결-authorprofilehead:

--- pages/artists/[nickname].tsx
+++ pages/artists/[nickname].tsx
@@ -163,7 +163,7 @@ const Artist = ({ artist_name2info, artist_artworks }) => {
         // position="relative"
         // overflow="hidden" // 모바일 사파리에서 여백이 생기는 문제 해결
       >
-        {/* <AuthorProfileHead nickname={nickname} profile={profile} /> */}
+        <AuthorProfileHead nickname={nickname} profile={profile} />
         <ViewSelectBar
           activeView={activeView}
           onViewChange={handleViewChange}
