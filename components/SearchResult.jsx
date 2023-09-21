035e9cb4c17b8fa68621754fd321dc845398ffca
chokoty <chokokty0816@gmail.com>
2023-09-22 04:25:22 +0900

    Feat: 검색 결과 작가프로필페이지 링크연결3

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -87,8 +87,7 @@ const SearchResult = ({
               profURL={author?.author_prof_url}
               nickname={author?.author_nickname}
               board={uploadTimeDiff}
-              // writerURL={author?.author_url}
-              writerURL={'/artists/' + author?.author_nickname}
+              writerURL={author?.author_url}
             />
           </Skeleton>
         </div>
