32980fe00d7e80fcb59de829fa28477577d64d72
chokoty <chokokty0816@gmail.com>
2023-12-24 03:53:40 +0900

    Fix: 작가페이지 정렬 버그수정3

--- pages/artists/[nickname].tsx
+++ pages/artists/[nickname].tsx
@@ -20,7 +20,7 @@ const Artist = ({ artist_name2info }) => {
   const { ref, inView } = useInView({
     // infinite scroll을 위한 옵저버
     threshold: 0,
-    rootMargin: '20px 0px', // 상단에서 800px 떨어진 지점에서 데이터를 불러옵니다. 이 값을 조정하여 원하는 위치에서 데이터를 불러올 수 있습니다.
+    rootMargin: '400px 0px', // 상단에서 800px 떨어진 지점에서 데이터를 불러옵니다. 이 값을 조정하여 원하는 위치에서 데이터를 불러올 수 있습니다.
   });
 
   const nickname = router.query.nickname as string;
