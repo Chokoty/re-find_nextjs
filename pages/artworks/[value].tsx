f5d7cbf7aa509518c92790586d9dfe5e82370300
chokoty <chokokty0816@gmail.com>
2024-01-02 21:27:53 +0900

    Fix: 멤버 팬아트 수 가리기2

--- pages/artworks/[value].tsx
+++ pages/artworks/[value].tsx
@@ -175,7 +175,7 @@ export default function Album({ value, query }) {
         {album?.description && <Text m="0 auto">{album.description}</Text>}
         {
           // member는 팬아트 개수 안 보이게
-          gallary && <Text>총 {total}개의 팬아트가 있습니다.</Text>
+          album && <Text>총 {total}개의 팬아트가 있습니다.</Text>
         }
         {/* <Text>총 {total}개의 팬아트가 있습니다.</Text> */}
       </Box>
