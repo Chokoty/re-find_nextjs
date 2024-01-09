4c64279ff7e5bf9a2ffe3b10b3960bcd328c4d56
chokoty <chokokty0816@gmail.com>
2024-01-09 15:06:40 +0900

    Fix: snowfall 조정

--- components/event/MySnowfall.tsx
+++ components/event/MySnowfall.tsx
@@ -1,15 +1,23 @@
+import { useColorModeValue } from '@chakra-ui/react';
 import Snowfall from 'react-snowfall';
 
+import { darkMode, lightMode } from '@/styles/theme';
+
 const MySnowfall = () => {
+  const color = useColorModeValue(lightMode.snowfall, darkMode.snowfall);
+
   return (
     <>
       <Snowfall
         // Changes the snowflake color
-        color="#dee4fd"
+        color={color}
         // Applied to the canvas element
         // style={{ background: '#fff' }}
         // Controls the number of snowflakes that are created (default 150)
         snowflakeCount={200}
+        speed={[0.5, 3]}
+        wind={[-0.3, 3]}
+        radius={[0.5, 5]}
       />
     </>
   );
