575972b5c8a73a901b28136fdb083cb7c2ac8d76
chokoty <chokokty0816@gmail.com>
2024-01-27 18:17:53 +0900

    Feat: 작품세부페이지 추가 1.1

--- components/card/MasonryCardIsdPick.tsx
+++ components/card/MasonryCardIsdPick.tsx
@@ -103,186 +103,186 @@ const MasonryCardIsdPick = ({
           //   className="link-to-wakzoo-from-profile"
           //   href={artwork?.source[0] ? article_link + artwork.source[0] : '#'}
           //   isExternal
+          // >
+          <Flex
+            flexDir="column"
+            position="absolute"
+            top={0}
+            right={0}
+            bottom={0}
+            left={0}
+            borderRadius="1rem"
+            zIndex={2}
+            background="rgba(0, 0, 0, 0.3)"
+            justifyContent="space-between"
+            alignItems="center"
+            color="white"
+            fontSize="2rem"
+            fontWeight="bold"
+            cursor="pointer"
+            p={['0.5rem ', '1rem 0']}
+            onMouseLeave={() => onToggleFocus(null)}
           >
-            <Flex
-              flexDir="column"
-              position="absolute"
-              top={0}
-              right={0}
-              bottom={0}
-              left={0}
-              borderRadius="1rem"
-              zIndex={2}
-              background="rgba(0, 0, 0, 0.3)"
-              justifyContent="space-between"
-              alignItems="center"
-              color="white"
-              fontSize="2rem"
-              fontWeight="bold"
-              cursor="pointer"
-              p={['0.5rem ', '1rem 0']}
-              onMouseLeave={() => onToggleFocus(null)}
+            <Text
+              fontSize={['sm', 'xl']}
+              fontWeight="600"
+              whiteSpace="nowrap"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              maxWidth="100%"
             >
-              <Text
-                fontSize={['sm', 'xl']}
-                fontWeight="600"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textOverflow="ellipsis"
-                maxWidth="100%"
-              >
-                {artwork.board}
-              </Text>
-              {imageHeight >= 212 && (
-                <>
-                  <Flex
-                    flexDir="column"
-                    justifyContent="center"
-                    alignItems="center"
-                  >
-                    <Text
-                      fontSize={['sm', 'xl']}
-                      fontWeight="400"
-                      whiteSpace="nowrap"
-                      overflow="hidden"
-                      textOverflow="ellipsis"
-                      maxWidth="100%"
-                      textAlign="center"
-                    >
-                      {/* {nickname} */}
-                      {artwork?.author}
-                    </Text>
-                    <Text
-                      fontSize={['sm', 'xl']}
-                      fontWeight="400"
-                      whiteSpace="nowrap"
-                      overflow="hidden"
-                      textOverflow="ellipsis"
-                      maxWidth="100%"
-                      textAlign="center"
-                    >
-                      {artwork?.date?.split(' ')[0].slice(0, -1)}
-                    </Text>
-                  </Flex>
-                  <Flex
-                    flexDir="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    textAlign="center"
-                    // w="90%"
-                    gap="1rem"
-                  >
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      justifyContent="center"
-                      alignItems="center"
-                      gap="0.3rem"
-                    >
-                      <FaEye style={iconStyle} />
-                      <Text fontSize={['sm', 'xl']} fontWeight="400">
-                        {formatArtistValue(artwork.view)}
-                      </Text>
-                    </Box>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      justifyContent="center"
-                      alignItems="center"
-                      gap="0.3rem"
-                    >
-                      <FaThumbsUp style={iconStyle} />
-                      <Text fontSize={['sm', 'xl']} fontWeight="400">
-                        {formatArtistValue(artwork.like)}
-                      </Text>
-                    </Box>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      justifyContent="center"
-                      alignItems="center"
-                      gap="0.3rem"
-                    >
-                      <FaComment style={iconStyle} />
-                      <Text fontSize={['sm', 'xl']} fontWeight="400">
-                        {formatArtistValue(artwork.comment)}
-                      </Text>
-                    </Box>
-                  </Flex>
-                </>
-              )}
-              {imageHeight < 212 && (
-                <>
-                  {imageHeight > 140 && (
-                    <Text
-                      fontSize={['xs', 'sm']}
-                      fontWeight="400"
-                      whiteSpace="nowrap"
-                      overflow="hidden"
-                      textOverflow="ellipsis"
-                      maxWidth="100%"
-                      textAlign="center"
-                    >
-                      {artwork?.date?.split(' ')[0].slice(0, -1)}
-                    </Text>
-                  )}
-                  <Flex
-                    flexDir="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    textAlign="center"
-                    // w="100%"
-                    gap="1rem"
-                  >
-                    <Box>
-                      <FaEye style={iconStyle} />
-                      <Text fontSize={['xs', 'sm']} fontWeight="400">
-                        {formatArtistValue(artwork.view)}
-                      </Text>
-                    </Box>
-                    <Box>
-                      <FaThumbsUp style={iconStyle} />
-                      <Text fontSize={['xs', 'sm']} fontWeight="400">
-                        {formatArtistValue(artwork.like)}
-                      </Text>
-                    </Box>
-                    <Box>
-                      <FaComment style={iconStyle} />
-                      <Text fontSize={['xs', 'sm']} fontWeight="400">
-                        {formatArtistValue(artwork.comment)}
-                      </Text>
-                    </Box>
-                  </Flex>
-                </>
-              )}
-              <Flex
-                w="100%"
-                flexDir="row"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Button
-                  as={Link}
-                  className="link_to_wakzoo"
-                  href={article_link + artwork.source[0]}
-                  isExternal
-                  _hover={{
-                    textDecoration: 'none',
-                    cursor: 'pointer',
-                    backgroundColor: 'green.400',
-                  }}
-                  target="_blank"
-                  rel="noopener noreferrer" // 보안상의 이유료 이 부분도 추가합니다.
-                  colorScheme="green"
-                  borderRadius="1rem"
-                  w="40%"
-                  h={['2.5rem', '3rem']}
+              {artwork.board}
+            </Text>
+            {imageHeight >= 212 && (
+              <>
+                <Flex
+                  flexDir="column"
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <Text fontSize={['xs', 'md']}>원본 게시글</Text> &nbsp;
-                  <HiOutlineExternalLink />
-                </Button>
-              </Flex>
+                  <Text
+                    fontSize={['sm', 'xl']}
+                    fontWeight="400"
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    maxWidth="100%"
+                    textAlign="center"
+                  >
+                    {/* {nickname} */}
+                    {artwork?.author}
+                  </Text>
+                  <Text
+                    fontSize={['sm', 'xl']}
+                    fontWeight="400"
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    maxWidth="100%"
+                    textAlign="center"
+                  >
+                    {artwork?.date?.split(' ')[0].slice(0, -1)}
+                  </Text>
+                </Flex>
+                <Flex
+                  flexDir="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  textAlign="center"
+                  // w="90%"
+                  gap="1rem"
+                >
+                  <Box
+                    display="flex"
+                    flexDir="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap="0.3rem"
+                  >
+                    <FaEye style={iconStyle} />
+                    <Text fontSize={['sm', 'xl']} fontWeight="400">
+                      {formatArtistValue(artwork.view)}
+                    </Text>
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDir="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap="0.3rem"
+                  >
+                    <FaThumbsUp style={iconStyle} />
+                    <Text fontSize={['sm', 'xl']} fontWeight="400">
+                      {formatArtistValue(artwork.like)}
+                    </Text>
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDir="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap="0.3rem"
+                  >
+                    <FaComment style={iconStyle} />
+                    <Text fontSize={['sm', 'xl']} fontWeight="400">
+                      {formatArtistValue(artwork.comment)}
+                    </Text>
+                  </Box>
+                </Flex>
+              </>
+            )}
+            {imageHeight < 212 && (
+              <>
+                {imageHeight > 140 && (
+                  <Text
+                    fontSize={['xs', 'sm']}
+                    fontWeight="400"
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    maxWidth="100%"
+                    textAlign="center"
+                  >
+                    {artwork?.date?.split(' ')[0].slice(0, -1)}
+                  </Text>
+                )}
+                <Flex
+                  flexDir="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  textAlign="center"
+                  // w="100%"
+                  gap="1rem"
+                >
+                  <Box>
+                    <FaEye style={iconStyle} />
+                    <Text fontSize={['xs', 'sm']} fontWeight="400">
+                      {formatArtistValue(artwork.view)}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <FaThumbsUp style={iconStyle} />
+                    <Text fontSize={['xs', 'sm']} fontWeight="400">
+                      {formatArtistValue(artwork.like)}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <FaComment style={iconStyle} />
+                    <Text fontSize={['xs', 'sm']} fontWeight="400">
+                      {formatArtistValue(artwork.comment)}
+                    </Text>
+                  </Box>
+                </Flex>
+              </>
+            )}
+            <Flex
+              w="100%"
+              flexDir="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button
+                as={Link}
+                className="link_to_wakzoo"
+                href={article_link + artwork.source[0]}
+                isExternal
+                _hover={{
+                  textDecoration: 'none',
+                  cursor: 'pointer',
+                  backgroundColor: 'green.400',
+                }}
+                target="_blank"
+                rel="noopener noreferrer" // 보안상의 이유료 이 부분도 추가합니다.
+                colorScheme="green"
+                borderRadius="1rem"
+                w="40%"
+                h={['2.5rem', '3rem']}
+              >
+                <Text fontSize={['xs', 'md']}>원본 게시글</Text> &nbsp;
+                <HiOutlineExternalLink />
+              </Button>
             </Flex>
+          </Flex>
           // </Link>
         )}
       </Box>
