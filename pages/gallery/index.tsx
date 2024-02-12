dfec4c2c5326e857574bc081e0d086f5c2f6e237
chokoty <chokokty0816@gmail.com>
2024-02-12 18:19:55 +0900

    Test: 카드테스트

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,17 +1,23 @@
-import { Box, flexbox, useColorModeValue, Text, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  flexbox,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
 import AlbumGrid from '@/components/artwork/albumGrid';
 import MemberButtonList from '@/components/artwork/MemberButtonList';
+import GalleryAlbum from '@/components/card/GalleryAlbum';
+import GalleryCard from '@/components/card/GalleryCard';
 import PageTitleIndex from '@/components/common/PageTitleIndex';
 import gallery from '@/data/gallery';
 import members from '@/data/members';
 import { useStore } from '@/store/store';
 import { darkMode, lightMode } from '@/styles/theme';
-import { MdOutlineArrowForwardIos } from 'react-icons/md';
-import GalleryCard from '@/components/card/GalleryCard';
-import GalleryAlbum from '@/components/card/GalleryAlbum';
 
 const Artworks = () => {
   const router = useRouter();
@@ -62,16 +68,10 @@ const Artworks = () => {
   };
 
   const callButton = (text) => (
-    <Button 
-      borderRadius="2rem"
-      mr="0.5rem"
-      opacity="0.7" 
-    >
-      <Text>
-        { text }
-      </Text>
+    <Button borderRadius="2rem" mr="0.5rem" opacity="0.7">
+      <Text>{text}</Text>
     </Button>
-  )//버튼 호출 함수
+  ); // 버튼 호출 함수
 
   return (
     <Box
@@ -86,149 +86,126 @@ const Artworks = () => {
       <Box w="100%" h="100%" style={backgroundStyle}></Box>
       <PageTitleIndex topTitle={topTitle} />
 
-
-
-      <Box 
-        w = "100%"
-        h = "800px"
-        ml ="6rem"
-      >
-        
+      <Box w="100%" h="800px" ml="6rem">
         <Box display="flex">
-          <Text 
-            textAlign="left"
-            fontWeight="bold"
-            fontSize="40px"
-            mr="1rem"
-          >
-              이 주의 왁물원 인기 팬아트!
+          <Text textAlign="left" fontWeight="bold" fontSize="40px" mr="1rem">
+            이 주의 왁물원 인기 팬아트!
           </Text>
-          <MdOutlineArrowForwardIos 
+          <MdOutlineArrowForwardIos
             style={{
-              width:"2rem",
-              height:"2rem",
-              marginTop:"1rem"
+              width: '2rem',
+              height: '2rem',
+              marginTop: '1rem',
             }}
           />
         </Box>
-  
+
         <Box
           display="flex"
           justifyContent="flex-start"
           alignItems="center"
           mt="2rem"
-        > 
+        >
           {callButton('전체')}
           {callButton('이세계아이돌')}
           {callButton('고정멤버')}
           {callButton('불법 배경')}
           {callButton('우왁굳')}
           {callButton('금손 작가들의 방')}
-        </Box>{/* 버튼 */}
+        </Box>
+        {/* 버튼 */}
 
         <Box display="flex" alignItems="center" gap="20px" mt="2rem">
           <GalleryCard
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryCard
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryCard
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryCard
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryCard
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-        </Box>{/* 사진들 */}
-
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryCard
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryCard
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryCard
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryCard
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+        </Box>
+        {/* 사진들 */}
       </Box>
 
-
-
-      <Box 
-        w = "100%"
-        h = "800px"
-        ml ="6rem"
-        mt="8rem"
-      >
+      <Box w="100%" h="800px" ml="6rem" mt="8rem">
         <Box display="flex">
-          <Text 
-            textAlign="left"
-            fontWeight="bold"
-            fontSize="40px"
-            mr="1rem"
-          >
+          <Text textAlign="left" fontWeight="bold" fontSize="40px" mr="1rem">
             리파인드 인기 앨범
           </Text>
-          <MdOutlineArrowForwardIos 
+          <MdOutlineArrowForwardIos
             style={{
-              width:"2rem",
-              height:"2rem",
-              marginTop:"1rem"
+              width: '2rem',
+              height: '2rem',
+              marginTop: '1rem',
             }}
           />
-        </Box>  
+        </Box>
 
         <Box
           display="flex"
           justifyContent="flex-start"
           alignItems="center"
           mt="2rem"
-        > 
+        >
           {callButton('전체')}
           {callButton('이세계아이돌')}
           {callButton('고정멤버')}
           {callButton('불법 배경')}
           {callButton('우왁굳')}
           {callButton('금손 작가들의 방')}
-        </Box>{/* 버튼 */}
-        
-        <Box display="flex" alignItems="center" gap="20px" mt="2rem">
-            <GalleryAlbum 
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryAlbum 
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryAlbum 
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
-            <GalleryAlbum 
-              writerURL="12312"
-              profURL="asdf"
-              nickname="nickname"
-              board={["board1", "board2", "board3"]}
-            />
         </Box>
-            
-      </Box>  
+        {/* 버튼 */}
+
+        <Box display="flex" alignItems="center" gap="20px" mt="2rem">
+          <GalleryAlbum
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryAlbum
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryAlbum
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+          <GalleryAlbum
+            writerURL="12312"
+            profURL="asdf"
+            nickname="nickname"
+            board={['board1', 'board2', 'board3']}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
