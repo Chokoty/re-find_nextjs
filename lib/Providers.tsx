589ceee160cfdb3065f72bd75fa73191ff20ce2f
shseok <shseok0674@gmail.com>
2024-03-06 13:11:48 +0900

    Setting: tanstack query 설정

--- lib/Providers.tsx
+++ lib/Providers.tsx
@@ -1,9 +1,30 @@
 'use client';
 
 import { ChakraProvider } from '@chakra-ui/provider';
+import { useState, PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import theme from '@/styles/theme';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+export function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000, // 60sec(1min)
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
