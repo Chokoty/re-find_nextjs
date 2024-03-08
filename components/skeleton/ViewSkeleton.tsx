47be7bb8c751d1ddff1d4c677e60af08a54d86bc
shseok <shseok0674@gmail.com>
2024-03-09 00:11:35 +0900

    Design: ViewSkeleton 개수 및 크기 수정

--- components/skeleton/ViewSkeleton.tsx
+++ components/skeleton/ViewSkeleton.tsx
@@ -45,20 +45,27 @@ export default function ViewSkeleton({ view }: Prop) {
         placeItems="center"
         m="0 auto"
       >
-        <Skeleton
-          m={['0', '0 0.5rem']}
-          mb=" 1rem"
-          w={['170px', '236px']}
-          h={['120px', '157px']}
-          overflow="hidden"
-          borderRadius="1rem"
-        />
-        <SkeletonText
-          mt="2" // 0.5rem
-          noOfLines={2}
-          spacing="1"
-          skeletonHeight="5"
-        />
+        {Array.from({ length: 15 }, (_, index) => (
+          <Box w="100%" key={index}>
+            <Skeleton
+              // m={['0', '0 0.5rem']}
+              mb="0.5rem"
+              w={['170px', '236px']}
+              h={['120px', '157px']}
+              overflow="hidden"
+              borderRadius="1rem"
+            />
+            <Box w="90%">
+              <SkeletonText
+                mt="1" // 0.5rem
+                noOfLines={2}
+                spacing="1"
+                skeletonHeight="5"
+                borderRadius="1rem"
+              />
+            </Box>
+          </Box>
+        ))}
       </SimpleGrid>
     </Box>
   );
