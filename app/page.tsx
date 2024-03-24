c1d883200b29573682000b2f19c54829e708c768
shseok <shseok0674@gmail.com>
2024-03-24 15:22:57 +0900

    Fix: 타임아웃걸리는 api Next 서버단 호출 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-import { QueryClient } from '@tanstack/query-core';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-
 import Home from '@/components/home/Home';
-import queryOptions from '@/service/client/home/queries';
-
-export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
-  const { queryKey: uKey, queryFn: uFn } = queryOptions.updates();
-  const { queryKey: cKey, queryFn: cFn } = queryOptions.counts();
-
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: uKey,
-    queryFn: uFn, // request 함수 내부에서 에러를 발생하면 prefetch만 실패하고 페이지는 렌더링 (따로 에러처리 x)
-    // ..
-  });
-
-  await queryClient.prefetchQuery({
-    queryKey: cKey,
-    queryFn: cFn,
-  });
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydratedState}>
-      <Home />
-    </HydrationBoundary>
-  );
+  return <Home />;
 }
