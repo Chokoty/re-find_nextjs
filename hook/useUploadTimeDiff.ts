328edd8642412b96b1cf1b3a786d51ec558cce20
chokoty <chokokty0816@gmail.com>
2024-04-08 23:23:29 +0900

    Chore: 중복 코드 제거, console 출력 제거

--- hook/useUploadTimeDiff.ts
+++ hook/useUploadTimeDiff.ts
@@ -11,35 +11,15 @@ export const useUploadTimeDiff = (writeDate: string): string => {
     const nowUTC = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
     const nowKR = new Date(nowUTC + KR_TIME_DIFF);
-    // Intl.DateTimeFormat을 사용하여 현재 한국 시간을 구합니다.
-    // const nowKRFormatter = new Intl.DateTimeFormat('ko-KR', {
-    //   year: 'numeric',
-    //   month: '2-digit',
-    //   day: '2-digit',
-    //   hour: '2-digit',
-    //   minute: '2-digit',
-    //   second: '2-digit',
-    //   timeZone: 'Asia/Seoul',
-    // });
-    // const nowKR2 = nowKRFormatter.format(new Date());
-
-    // console.log(nowKR2);
-    // console.log(nowKR);
-
-    // const uploadedDate = new Date(writeDate);
-
     const parts = writeDate.split(/[\s.:]+/);
-    // console.log(parts);
-    // if (parts.length >= 5) {
+
     const year = parseInt(parts[0]);
     const month = parseInt(parts[1]) - 1;
     const day = parseInt(parts[2]);
     const hour = parseInt(parts[3]);
     const minute = parseInt(parts[4]);
-    const date = new Date(year, month, day, hour, minute);
-
-    // console.log(date);
-    const uploadedDate = date;
+    const uploadedDate = new Date(year, month, day, hour, minute);
+    // const uploadedDate = new Date(writeDate);
 
     const timeDifference = nowKR.getTime() - uploadedDate.getTime();
     const secondsDifference = Math.floor(timeDifference / 1000);
