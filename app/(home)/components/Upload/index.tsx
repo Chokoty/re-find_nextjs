16a72a0bae032ad0bfa81ee93433166ec92d5940
shseok <shseok0674@gmail.com>
2024-04-15 15:33:58 +0900

    Fix: home이동시 업로드 컴포넌트 초기화

--- app/(home)/components/Upload/index.tsx
+++ app/(home)/components/Upload/index.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react';
 // import { useDisclosure, useToast } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 import { useImageUploadStore } from '@/app/(home)/store/imageUploadStore';
@@ -8,12 +9,18 @@ import BeforeUpload from './BeforeUpload';
 import ImageViewer from './ImageViewer';
 
 export default function Upload() {
-  const { hashs, uploadedfiles } = useImageUploadStore(
+  const { hashs, uploadedfiles, resetFiles } = useImageUploadStore(
     useShallow((state) => ({
       hashs: state.hashs,
       uploadedfiles: state.uploadedfiles,
+      resetFiles: state.resetFiles,
     }))
   );
+
+  // 다른 페이지로 이동할 때 초기화 (해당 페이지로 돌아왔을 때 초기화 되어있어야 함)
+  useEffect(() => {
+    resetFiles();
+  }, []);
 
   return (
     <>
