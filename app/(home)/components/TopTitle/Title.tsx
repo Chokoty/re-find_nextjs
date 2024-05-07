1cdcc4e1bc082fb1d32867717bafe8ddb3b4c781
shseok <shseok0674@gmail.com>
2024-05-07 17:19:13 +0900

    Refactor: 폰트 최적화

--- app/(home)/components/TopTitle/Title.tsx
+++ app/(home)/components/TopTitle/Title.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
 
-// TODO: ONE-Mobile-POP 폰트 최적화 필요 feat) fonts.ts
 export default function Title() {
   return (
     <Link href="/" className="no-underline">
-      <h2 className="flex items-center justify-center text-center font-['ONE-Mobile-POP'] text-[60px] font-bold leading-tight 2xs:text-[70px] md:text-[85px] lg:text-[100px]">
+      <h2 className="font-pop flex items-center justify-center text-center text-[60px] font-bold leading-tight 2xs:text-[70px] md:text-[85px] lg:text-[100px]">
         <span className="text-green-highlight dark:text-pink-highlight">
           RE:
         </span>
--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -8,7 +8,7 @@ type Prop = {
 export default function PageTitle({ topTitle }: Prop) {
   return (
     <div className="mx-auto mt-2.5">
-      <h1 className="text-center font-['ONE-Mobile-POP'] text-3xl font-normal 2xs:text-5xl">
+      <h1 className="text-center font-pop text-3xl font-normal 2xs:text-5xl">
         {topTitle.title}
       </h1>
       <p className="pt-4 text-center text-sm font-normal 2xs:text-base">
