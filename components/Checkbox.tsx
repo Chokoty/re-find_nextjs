c47802d28288e6003527c11bbe9dbb7564229f34
shseok <shseok0674@gmail.com>
2024-06-06 16:22:30 +0900

    Design(Re): Checkbox에 삐져나온 input을 숨김

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -15,8 +15,7 @@ export default function Checkbox({
     <label className="relative inline-flex cursor-pointer items-center align-top">
       <input
         name={name}
-        className="absolute -m-px size-px overflow-hidden whitespace-nowrap border-0 border-none p-0"
-        style={{ clip: 'rect(0px, 0px, 0px, 0px);' }}
+        className="absolute -m-px size-0 overflow-hidden whitespace-nowrap border-0 border-none p-0"
         type="checkbox"
         onChange={onChange}
         defaultChecked={defaultChecked}
