994dc4980a6f91620762cc5ae33c4508e6ea299b
shseok <shseok0674@gmail.com>
2024-06-07 19:52:14 +0900

    Design: 검색 페이지 전체화면 로딩 overlay 제거

--- app/search/components/Loading.tsx
+++ app/search/components/Loading.tsx
@@ -14,7 +14,7 @@ export default function Loading() {
 
   return (
     isShow && (
-      <div className="fixed inset-0 z-[299] flex size-full items-center justify-center bg-blackAlpha-300">
+      <div className="fixed left-1/2 top-1/2 z-[299] -translate-x-1/2 -translate-y-1/2">
         <ClimbingBoxLoader color="#01BFA2" />
       </div>
     )
