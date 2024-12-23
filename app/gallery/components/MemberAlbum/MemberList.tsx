b7441f9411bd10a73a28620caa1621234e923920
chokoty <chokokty0816@gmail.com>
2024-12-24 04:02:27 +0900

    Fix: 멤버 카드 디자인 수정

--- app/gallery/components/MemberAlbum/MemberList.tsx
+++ app/gallery/components/MemberAlbum/MemberList.tsx
@@ -24,14 +24,7 @@ const Member = ({ member }: { member: MemberType }) => {
         // href={`/gallery/${value}`}
         href={`/gallery/${value}?viewType=masonry&sortType=latest`}
         prefetch={false}
-        className="flex size-full flex-col items-center  justify-center    gap-4 rounded-md p-4 transition hover:bg-gray-200 active:bg-whiteAlpha-400 dark:hover:bg-whiteAlpha-300 dark:active:bg-black-200 "
-        // style={{
-        //   display: 'flex',
-        //   flexDirection: 'column',
-        //   gap: '7px',
-        //   justifyContent: 'center',
-        //   alignItems: 'center',
-        // }}
+        className="flex size-full flex-col items-center  justify-center    gap-4 rounded-md p-2 transition hover:bg-gray-200 active:bg-whiteAlpha-400 dark:hover:bg-whiteAlpha-300 dark:active:bg-black-200 md:p-4 "
       >
         <div className="relative size-[85px] md:size-[160px]">
           <Image
@@ -45,8 +38,10 @@ const Member = ({ member }: { member: MemberType }) => {
             unoptimized
           />
         </div>
-        <p className=" w-full text-start">{name}</p>
-        <p className="w-full text-start text-xs">{`"${greetings}"`}</p>
+        <div className="w-full">
+          <p className="w-full text-start">{name}</p>
+          <p className="w-full text-start text-[14px] leading-6  text-blackAlpha-600 dark:text-whiteAlpha-600">{`"${greetings}"`}</p>
+        </div>
       </Link>
     </li>
   );
