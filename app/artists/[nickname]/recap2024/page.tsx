6e2c41f60a781180c06988a4b095d4f610498f76
chokoty <chokokty0816@gmail.com>
2025-01-09 22:28:35 +0900

    Feat: 리캡페이지 예외처리

--- app/artists/[nickname]/recap2024/page.tsx
+++ app/artists/[nickname]/recap2024/page.tsx
@@ -26,6 +26,19 @@ export default async function Recap2024({ params: { nickname } }: Params) {
     await getAuthorRecapResults(nickname);
   // const bestOfYearInfo = await getBestOfYearFanartInfo(nickname);
 
+  // statistics가 없을 경우 메시지 표시
+  if (!statistics) {
+    return (
+      <div className="flex size-full h-screen w-full flex-col items-center justify-start bg-recap-pattern p-8 py-16 text-whiteAlpha-900">
+        <BackToArtistButton />
+
+        <p className="my-10 text-2xl font-semibold text-white  2xs:text-4xl">
+          {`${decodeURIComponent(nickname)} 작가님은 2024년에 팬아트를 올리지 않았습니다.`}
+        </p>
+      </div>
+    );
+  }
+
   const data = [
     {
       title: '총 조회 수',
