8a429e3b10ffb0620f75b316f0533a7e2b4af3b6
chokoty <chokokty0816@gmail.com>
2025-01-14 19:57:30 +0900

    Feat: 리캡페이지 설명추가

--- app/recap2024/components/TopContent.tsx
+++ app/recap2024/components/TopContent.tsx
@@ -16,11 +16,22 @@ export default function TopContent({
 }) {
   return (
     <div className="m-auto flex w-full max-w-screen-xl flex-col items-center justify-between lg:w-[90%]">
+      <div className="m-8 flex w-full flex-col items-center justify-center gap-2 text-start  text-base lg:text-3xl ">
+        <p className="w-full">
+          신년을 맞이하여 왁물원 팬아트 2024 리캡 페이지를 만들었어요.
+        </p>
+        <p className="w-full">
+          작가님 프로필 페이지에서 작가님들의 개인 리캡도 확인해보세요!
+        </p>
+        <p className="w-full">2025년에도 작가님들의 활동을 응원합니다! 킹아!</p>
+      </div>
+
       <div className="mt-8 flex w-full flex-col items-start justify-between gap-16 lg:gap-4 xl:flex-row xl:items-center">
         <Title artist={artist} />
         <Counter total={total} isAuthor={!!artist} />
       </div>
       {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2"> */}
+
       <div className="mb-4 mt-14 flex flex-wrap justify-center gap-4 lg:mt-28">
         {statistics.map((item, index) => (
           <StatisticCard key={index} item={item} />
