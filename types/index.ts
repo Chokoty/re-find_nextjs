af9e0ba3cfbf010321e20f902a9e3593bbdc6913
chokoty <chokokty0816@gmail.com>
2025-01-17 17:31:07 +0900

    Fix: vercel 오류

--- types/index.ts
+++ types/index.ts
@@ -23,6 +23,8 @@ export type FileWithPreview = File & {
   preview: string;
 };
 
+export type GetLoginQueryParams = { code: string | null; state: string | null };
+
 // artists
 export type SortTotalCriteria = keyof Pick<
   AuthorCommon,
