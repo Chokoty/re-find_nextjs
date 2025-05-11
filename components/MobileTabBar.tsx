c2d7947370878589afa7aec0e6aa0cd66b0389fc
shseok <shseok0674@gmail.com>
2025-05-11 16:44:42 +0900

    Design: 모바일 하단 탭바 아이콘 변경

--- components/MobileTabBar.tsx
+++ components/MobileTabBar.tsx
@@ -4,10 +4,9 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-import { FaUserGroup } from 'react-icons/fa6';
-import { IoMdImages } from 'react-icons/io';
-import { MdHomeFilled } from 'react-icons/md';
+import { FiFolder, FiUsers } from 'react-icons/fi';
+import { IoSearchOutline } from 'react-icons/io5';
+import { LiaHomeSolid } from 'react-icons/lia';
 
 import { useDeleteModeStore } from '@/app/album/store/deleteModeStore';
 import { useSelectModeStore } from '@/app/album/store/selectModeStore';
@@ -20,50 +19,30 @@ const routerMap = {
     type: 'link',
     path: '/',
     name: '홈',
-    icon: MdHomeFilled,
+    icon: LiaHomeSolid,
     className: 'size-7',
   },
   search: {
     type: 'link',
     path: '/search',
     name: '검색',
-    icon: FaSearch,
-    className: 'size-6',
+    icon: IoSearchOutline,
+    className: 'size-7',
   },
-  // gallery: {
-  //   type: 'link',
-  //   path: '/album',
-  //   name: '갤러리',
-  //   icon: IoMdImages,
-  //   className: 'size-7',
-  // },
   artists: {
     type: 'link',
     path: '/artists',
     name: '작가',
-    icon: FaUserGroup,
+    icon: FiUsers,
     className: 'size-6',
   },
   album: {
     type: 'button',
     path: '/myLibrary',
     name: '내 라이브러리',
-    icon: IoMdImages,
-    className: 'size-7',
+    icon: FiFolder,
+    className: 'size-6',
   },
-  // me: {
-  //   type: 'button',
-  //   path: '/myLibrary',
-  //   name: '나',
-  //   icon: FaUserCircle,
-  //   className: 'size-6',
-  // },
-  // gallery: {
-  //   path: '/myLibrary',
-  //   name: '내 라이브러리',
-  //   icon: IoMdImages,
-  //   className: 'size-7',
-  // },
 };
 
 export default function MobileTabBar() {
@@ -113,11 +92,22 @@ export default function MobileTabBar() {
               : currentPathname.startsWith(eachPath);
 
           const tabClassName = clsx(
-            'flex size-12 flex-col items-center justify-center rounded-full active:bg-blackAlpha-100 dark:active:bg-whiteAlpha-300',
+            'flex size-16 flex-col items-center justify-center gap-0.5 active:bg-blackAlpha-100 dark:active:bg-whiteAlpha-300',
             {
               'text-gray-700 dark:text-white': isActive,
               'text-blackAlpha-500 dark:text-whiteAlpha-500': !isActive,
             }
+          );
+
+          const iconWrapperClass = 'flex size-7 items-center justify-center';
+
+          const content = (
+            <>
+              <div className={iconWrapperClass}>
+                <Icon className={iconStyle} />
+              </div>
+              <span className="w-20 text-center text-xs">{name}</span>
+            </>
           );
 
           if (type === 'button') {
@@ -125,33 +115,16 @@ export default function MobileTabBar() {
               <button
                 key={name}
                 className={tabClassName}
-                onClick={() => {
-                  handleClick(eachPath);
-                  // router.push(`/${eachPath}`) > 유저가 로그인되어있을시
-                }}
+                onClick={() => handleClick(eachPath)}
+                type="button"
               >
-                <Icon className={iconStyle} />
-                <span className="text-xs">{name}</span>
+                {content}
               </button>
             );
           }
           return (
-            <Link href={eachPath} key={name}>
-              {/* <div className={tabClassName}> */}
-              <div
-                className={clsx(
-                  'flex size-16 flex-col items-center justify-center gap-0.5 rounded-full active:bg-blackAlpha-100 dark:active:bg-whiteAlpha-300',
-                  {
-                    'text-gray-700 dark:text-white': isActive,
-                    'text-blackAlpha-500 dark:text-whiteAlpha-500': !isActive,
-                  }
-                )}
-              >
-                <div className="flex size-7 items-center justify-center">
-                  <Icon className={iconStyle} />
-                </div>
-                <span className="w-20 text-center text-xs">{name}</span>
-              </div>
+            <Link href={eachPath} key={name} className={tabClassName}>
+              {content}
             </Link>
           );
         })}
