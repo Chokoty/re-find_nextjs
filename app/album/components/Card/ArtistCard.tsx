00fb7be28e5f315dca82d418ee834da31628dca9
shseok <shseok0674@gmail.com>
2025-05-21 22:01:48 +0900

    Chore: 오타 수정

--- app/album/components/Card/ArtistCard.tsx
+++ app/album/components/Card/ArtistCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function ArtistCard({ artist }: { artist: SbuscribedArtist }) {
+export default function ArtistCard({ artist }: { artist: SubscribedArtist }) {
   const { nick, profimg } = artist;
   return (
     <Link
