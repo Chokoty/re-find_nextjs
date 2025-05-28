48a3a3239f72c302c986d5594a0f98145abbb567
shseok <shseok0674@gmail.com>
2025-05-28 20:14:08 +0900

    Chore: prev_loc 도메인 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,15 +12,17 @@ const redirect_uri = `${
     : process.env.NEXT_PUBLIC_CLIENT_URL
 }/register`;
 const client_id = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-const testApiBase = process.env.NEXT_PUBLIC_DEV_CLIENT_URL;
+const clientUrl = process.env.NEXT_PUBLIC_IS_LOCAL
+  ? process.env.NEXT_PUBLIC_DEV_CLIENT_URL
+  : process.env.NEXT_PUBLIC_CLIENT_URL;
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export default function LoginPage() {
-  const pathname = usePathname();
+  const pathname = '/';
   const goToNaverLogin = () => {
     // window.location.href = `https://nid.naver.com/oauth2.0/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&state=abcd`;
-    console.log(`${serverUrl}/v2/authorize?prev_loc=${testApiBase + pathname}`);
-    window.location.href = `${serverUrl}/v2/authorize?prev_loc=${testApiBase + pathname}`;
+    console.log(`${serverUrl}/v2/authorize?prev_loc=${clientUrl + pathname}`);
+    window.location.href = `${serverUrl}/v2/authorize?prev_loc=${clientUrl + pathname}`;
   };
 
   return (
