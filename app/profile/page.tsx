56dc266ec10703a26acb79b47d54f98359457fbd
shseok <shseok0674@gmail.com>
2025-05-30 22:00:09 +0900

    Feat: 모바일 프로필 로그아웃 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,21 @@
+import LogoutButton from '@/app/profile/components/LogoutButton';
 import UserProfile from '@/app/profile/components/UserProfile';
 
 export default function MyLibrary() {
   return (
-    <div className="w-full p-2">
-      <div className="w-full rounded-xl border-[1px] border-dark-myText bg-white shadow-sm dark:border-0 dark:bg-dark-card">
+    <div className="flex h-[calc(100vh-100px)] w-full flex-col items-center justify-between rounded-xl border-[1px] border-dark-myText bg-white p-2 shadow-sm dark:border-0 dark:bg-dark-card">
+      <div className="w-full">
         <div className="w-full pt-4">
-          <h1 className="mb-10 mt-1.5 break-keep p-10 font-pop text-5xl sm:text-5xl 2md:text-5xl lg:text-5xl 2xl:text-5xl">
+          <h1 className="mb-10 mt-1.5 break-keep p-5 font-pop text-5xl sm:text-5xl md:p-10 2md:text-5xl lg:text-5xl 2xl:text-5xl">
             프로필 설정
           </h1>
         </div>
-        <UserProfile />
+        <div className="px-4 md:p-0">
+          <UserProfile />
+        </div>
+      </div>
+      <div className="w-full md:hidden">
+        <LogoutButton />
       </div>
     </div>
   );
